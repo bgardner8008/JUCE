c6b4a3525aba3f1c8ced98e4dce6c1e54139cb24
Bill Gardner <billg@wavearts.com>
2020-04-21 19:09:43 -0400

    updates to fix juce5 on Xcode 11.4, see juce commit dddeb1ad68f8539f5ea2fbac36c07532f12bf9cf

--- modules/juce_audio_plugin_client/AU/juce_AUv3_Wrapper.mm
+++ modules/juce_audio_plugin_client/AU/juce_AUv3_Wrapper.mm
@@ -1116,7 +1116,7 @@ private:
         AudioBufferList* bufferList = nullptr;
         int maxFrames, numberOfChannels;
         bool isInterleaved;
-        AudioBuffer<float> scratchBuffer;
+        juce::AudioBuffer<float> scratchBuffer;
     };
 
     //==============================================================================
@@ -1542,7 +1542,7 @@ private:
         return noErr;
     }
 
-    void processBlock (AudioBuffer<float>& buffer, MidiBuffer& midiBuffer) noexcept
+    void processBlock (juce::AudioBuffer<float>& buffer, MidiBuffer& midiBuffer) noexcept
     {
         auto& processor = getAudioProcessor();
         const ScopedLock sl (processor.getCallbackLock());
